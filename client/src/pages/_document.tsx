e16a0db39921a887e009836fac38d2711d3634d8
draihan <draihan@student.ubc.ca>
2023-09-30 04:38:07 -0700

    Inital Commit NextJS boilerplate

---
+++ client/src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export const metadata = {
+  title: "Document Search",
+  description: "Will be defined soon",
+};
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
