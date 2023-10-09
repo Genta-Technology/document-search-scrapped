3c947962ca8c305cc246791a7f1c27c08d9b6d8d
draihan <draihan@student.ubc.ca>
2023-10-08 21:51:58 -0700

    Add PDF Viewer and Highlighter

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -12,7 +12,10 @@ import {
   Card,
   CardBody,
 } from "@nextui-org/react";
+
 import FileUploader from "@components/DropFile";
+
+import FileViewer from "@components/FileViewer";
 
 const MockModels = [
   { label: "Xenova/distilbert-base-cased-distilled-squad" },
@@ -85,6 +88,9 @@ const Home = () => {
       </div>
 
       <FileUploader />
+      <div className="bg-slate-400">
+        <FileViewer />
+      </div>
 
       <Chip>(placeholder for PDF viewer) Context: </Chip>
       <Card>
