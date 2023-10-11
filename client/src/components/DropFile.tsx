41c4cf547be61210e68a45a98193d119ecce9422
draihan <draihan@student.ubc.ca>
2023-10-10 20:09:18 -0700

    Parse PDF into text but left out the last page

--- client/src/components/DropFile.tsx
+++ client/src/components/DropFile.tsx
@@ -1,31 +1,17 @@
-import React, { useState } from "react";
-import { FileUploader } from "react-drag-drop-files";
+"use client";
 
-const fileTypes = ["PDF"];
+import { FilePond } from "react-filepond";
+import "filepond/dist/filepond.min.css";
+import { useState } from "react";
 
-const DropFile = () => {
-  const [file, setFile] = useState<File>();
-  const [isUploaded, setIsUploaded] = useState<boolean>();
-
-  const handleChange = (file: File) => {
-    setFile(file);
-  };
-  const handleFileUploaded = (file: File) => {
-    setIsUploaded(true);
-  };
+export default function DropFile() {
   return (
-    <div className="w-[25vw]">
-      <FileUploader
-        multiple={false}
-        handleChange={handleChange}
-        name="file"
-        types={fileTypes}
-        label={"Drop a File"}
-        className="max-w-max"
-        onDrop={handleFileUploaded}
-      />
-    </div>
+    <FilePond
+      server={{
+        process: "/api/upload",
+        fetch: null,
+        revert: null,
+      }}
+    />
   );
-};
-
-export default DropFile;
+}
