9be946b46f123fee796aedccb38a262939fad0e6
Restyled.io <commits@restyled.io>
2023-10-15 07:12:41 +0000

    Restyled by clang-format

--- client/next.config.js
+++ client/next.config.js
@@ -1,30 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode : true,
 
   // pdfjs
-  experimental: {
-    appDir: true,
+  experimental : {
+    appDir : true,
   },
-  future: { webpack5: true },
+  future : {webpack5 : true},
 
-    // (Optional) Export as a static site
-    // See https://nextjs.org/docs/pages/building-your-application/deploying/static-exports#configuration
-    // output: 'export', // Feel free to modify/remove this option
+  // (Optional) Export as a static site
+  // See
+  // https://nextjs.org/docs/pages/building-your-application/deploying/static-exports#configuration
+  // output: 'export', // Feel free to modify/remove this option
 
-    // Override the default webpack configuration
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-      // See https://webpack.js.org/configuration/resolve/#resolvealias
-      config.resolve.alias = {
-          ...config.resolve.alias,
-          "sharp$": false,
-          "onnxruntime-node$": false,
-      }
-      
-      config.resolve.alias.canvas = false
-      config.resolve.alias.encoding = false
-      return config;
+  // Override the default webpack configuration
+  webpack : (config, {buildId, dev, isServer, defaultLoaders, webpack}) => {
+    // See https://webpack.js.org/configuration/resolve/#resolvealias
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "sharp$" : false,
+      "onnxruntime-node$" : false,
+    }
+
+                           config.resolve.alias.canvas = false
+    config.resolve.alias.encoding = false
+    return config;
   },
 }
 
-module.exports = nextConfig
+                   module.exports = nextConfig
